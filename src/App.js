69b19a07bfc063c538e7c042b66fe0eec65de5f8
Nattapatscz <bigboyzin88@gmail.com>
2024-02-28 20:28:24 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
-import "./Questionaire.css"
+// import "./Questionaire.css"
 import HomeScreen from "./page/HomeScreen";
 import EnterID from "./page/EnterID";
 import Form from "./page/Form";
 import ShowInfo from "./page/ShowInfo";
 import PageQuestion1 from "./Question/PageQuestion1";
+import LoginForm from './Component/LoginForm'
+import Dashboard from './page/Dashboard';
+import Editting from './page/Editting';
+import Layout from './Component/Shared/Layout';
 import { BrowserRouter,Routes,Route,} from "react-router-dom";
+import Register from "./Component/Register";
 function App() {
  
 
@@ -18,6 +23,13 @@ function App() {
                     <Route exact path="/Form" element={<Form/>} />
                     <Route exact path="/ShowInfo" element={<ShowInfo/>} />
                     <Route exact path="/PageQuestion1" element={<PageQuestion1/>} />
+
+
+                    <Route exact path="/login" element={<LoginForm />} />
+                    <Route exact path="/Register" element={<Register/>} />
+                    <Route exact path="/Layout" element={<Layout/>} />
+                    <Route exact path="/Dashboard" element={<Dashboard/>} />
+                    <Route exact path="/Editting" element={<Editting/>} />
                 </Routes>
            </BrowserRouter>
     </>
